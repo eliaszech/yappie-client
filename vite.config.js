75cbbfad2206de940967841ce5264d88c1a35172
elias <elias.zech@outlook.com>
2026-05-15 00:34:35 +0200

    Add Docker support with production-ready configuration
    - Added `.dockerignore` to exclude local and build-specific files during Docker builds.
    - Created `Dockerfile` to build and serve the app using Node.js and NGINX in a multi-stage build process.
    - Added `nginx.conf` for handling static files, API proxying, and WebSocket support.
    - Updated `vite.config.js` to configure host whitelisting and API proxy for local development.

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,12 @@ export default defineConfig({
       tailwindcss(),
   ],
     server: {
-      allowedHosts: 'all',
+      allowedHosts: [
+          'e682-2a04-ee41-7-e023-1c1b-bfe4-be26-c10f.ngrok-free.app'
+      ],
+        proxy: {
+            '/api': 'http://localhost:8000',
+            '/broadcasting': 'http://localhost:8000',
+        }
     }
 })
