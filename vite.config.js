38d84842bbc22e4a1a09f0b86a4a4bb98b032b0c
elias <elias.zech@outlook.com>
2026-05-15 02:30:43 +0200

    Simplify dev server configuration: allow all hosts and remove API proxy settings

--- vite.config.js
+++ vite.config.js
@@ -9,12 +9,6 @@ export default defineConfig({
       tailwindcss(),
   ],
     server: {
-      allowedHosts: [
-          'e682-2a04-ee41-7-e023-1c1b-bfe4-be26-c10f.ngrok-free.app'
-      ],
-        proxy: {
-            '/api': 'http://localhost:8000',
-            '/broadcasting': 'http://localhost:8000',
-        }
+      allowedHosts: 'all',
     }
 })
